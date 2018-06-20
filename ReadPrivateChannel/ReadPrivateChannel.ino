ffb22056ab97ea36e2e5db879b586870d9e5f07f
faez.ham <faez.ham@gmail.com>
2018-06-20 16:20:38 +0430

    read and write

--- ReadPrivateChannel/ReadPrivateChannel.ino
+++ ReadPrivateChannel/ReadPrivateChannel.ino
@@ -1,6 +1,9 @@
-#include "ThingSpeak.h"
 #include <ESP8266WiFi.h>
+#include "ThingSpeak.h"
+#include "DHT.h"
 
+#define DHTTYPE DHT11
+#define DHTPIN D2 
 #if !defined(USE_WIFI101_SHIELD) && !defined(USE_ETHERNET_SHIELD) && !defined(ARDUINO_SAMD_MKR1000) && !defined(ARDUINO_AVR_YUN) && !defined(ARDUINO_ARCH_ESP8266) && !defined(ARDUINO_ARCH_ESP32)
   #error "Uncomment the #define for either USE_WIFI101_SHIELD or USE_ETHERNET_SHIELD"
 #endif
@@ -10,30 +13,52 @@ char pass[] = "faez22338510";
 int status = WL_IDLE_STATUS;
 WiFiClient  client;
 
-unsigned long lastConnectionTime = 0; 
+DHT dht(DHTPIN, DHTTYPE);
+
+unsigned long lastPostConnectionTime = 0;
+unsigned long lastRiciveConnectionTime = 0;  
 const unsigned long postingInterval = 10L * 1000L; // Post data every 10 seconds.
+const unsigned long RiciveInterval = 5L * 1000L; // Post data every 10 seconds.
 
 unsigned long myReadChannelNumber = 521815;
 const char * myReadAPIKey = "CXOHVN4Z7SCYQBU5";
 unsigned long myWriteChannelNumber = 516542;
-const char * myWriteAPIKey = "HDX272C9ML6S0I9B";
+const char * myWriteAPIKey = "H499QAGGZLC2SHIZ";
 
 void setup() {
-
-
   Serial.begin(115200);
+  pinMode(D1,OUTPUT);
+  dht.begin();
   WiFi.begin(ssid, pass);
   ThingSpeak.begin(client);
 }
 
 void loop() {
   // Read the latest value from field 1 of channel 31461
-  if (millis() - lastConnectionTime > postingInterval) {
-    float temp = ThingSpeak.readFloatField(myReadChannelNumber, 1, myReadAPIKey); 
-    Serial.print("Latest TEMP is: "); 
-    Serial.print(temp);
-    Serial.println("temp"); 
-    lastConnectionTime = millis();
-  }
+  if (millis() - lastPostConnectionTime > postingInterval) {
+    float t = dht.readTemperature(); 
+    float h = dht.readHumidity(); 
+    String data = String("field1=" + String(t, DEC) + "&field2=" + String(h, DEC));
+    Serial.println(data); 
+    ThingSpeak.setField(1,t);
+    ThingSpeak.setField(2,h);
+    ThingSpeak.writeFields(myWriteChannelNumber, myWriteAPIKey);  
 
+
+    lastPostConnectionTime = millis();
+  }
+  if (millis() - lastRiciveConnectionTime > RiciveInterval) {
+    float light = ThingSpeak.readFloatField(myReadChannelNumber, 1, myReadAPIKey);
+    if(light == 1){
+      digitalWrite(D1,1);
+    }
+    else{
+      digitalWrite(D1,0);
+    }
+    Serial.print("Latest Conroller value is: "); 
+    Serial.print(light);
+    Serial.println(""); 
+    lastRiciveConnectionTime = millis();
+  }
+//RiciveInterval
 }
